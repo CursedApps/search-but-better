aeab7a9542367c31daf50889912a37573d00bc93
Rose Hirigoyen <rosehirigoyen@gmail.com>
2020-11-21 01:00:33 -0500

    ctrl+f

--- background.js
+++ background.js
@@ -1,14 +1,12 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+  console.log("WORKING")
+  
+  window.addEventListener("keydown", (event) => { /* Add keydown too */
+    if (event.ctrlKey && event.shiftKey && event.key == 'F') {
+
+      // Block CTRL + F event
+      text = window.getSelection().toString();
+      content = document.documentElement.innerHTML
+      alert("WANNA SEARCH? " + text)
+
+    }
+  })
--- options.js
+++ options.js
@@ -4,11 +4,6 @@ function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
     button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
     page.appendChild(button);
   }
 }
